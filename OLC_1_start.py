d72104bc91655f2dfa83b04c6b455c2dcf90fb2d
Saanjhaay <saanjhaaygenius@gmail.com>
2024-11-18 08:59:01 +0000

    Co-authored-by: DLEE <autom8ton@users.noreply.github.com>

---
+++ OLC_1_start.py
@@ -0,0 +1,64 @@
+# num1 = 500
+# num2 = 700
+# answer = num1 + num2
+# print("num1 + num2=" ,answer)
+
+# name = input("what is your name")
+# command = input("what is your command?")
+# print(name , " command is", command)
+
+# print("test")
+
+# name = input("what is your name")
+# test = input("what is your command?")
+# print(name , " command is", test)
+
+# num1 = int(input("What is a number?"))
+# num2 = int(input("What is a number?"))
+# answer = num1 + num2
+# print(answer)  
+
+
+# for i in range(10):
+#     print("hello")
+
+# for i in range(2):
+#     for i in range(3):
+#         print("hello")
+#     for i in range(3):
+#         print("bye")
+
+# name = "Saanjhaay"
+# for i in name:
+#     print("Give me a" , i)
+#     print(i,"!!")
+# print("WHO IS THE BEST????")
+# print(name, "!!!")
+
+
+# for i in range(6, 73, 6):
+#     print(i)
+
+# for i in range(15 , 0, -1):
+#     print(i)
+
+# name = input("What is your name?")
+# allowance =   int(input("Whats is your weelkly allowance?"))
+# year = allowance*52
+# school = year *10
+# print(name)
+# print("Your weekly allowance is",allowance)
+# print("Your yearly allowance is", year)
+# print("Your entire allowance is" ,school)
+
+# fname= input("Enter you first name?")
+# lname = input("Enter your last name?")
+# total = len(fname) + len(lname)
+# print(total)
+
+import random
+
+for i in range(6):
+    num = random.randint(1, 47)
+    print(num)
+#tigeitgjseto5g4xj89cgtvhn h   
---
+++ hello.py
@@ -0,0 +1,19 @@
+# num1 = 500
+# num2 = 700
+# answer = num1 + num2
+# print("num1 + num2=" ,answer)
+
+# name = input("what is your name")
+# command = input("what is your command?")
+# print(name , " command is", command)
+
+# print("test")
+
+# name = input("what is your name")
+# test = input("what is your command?")
+# print(name , " command is", test)
+
+num1 = int(input("What is a number?"))
+num2 = int(input("What is a number?"))
+answer = num1 + num2
+print(answer)
